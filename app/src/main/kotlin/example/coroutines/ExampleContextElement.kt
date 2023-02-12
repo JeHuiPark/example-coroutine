8a4d71c02fa8ad6fe2a9db5738fbd033df0b98bf
jehuipark <pjh2359@gmail.com>
2023-02-12 15:56:36 +0900

    Add ExampleContextElement

---
+++ app/src/main/kotlin/example/coroutines/ExampleContextElement.kt
@@ -0,0 +1,39 @@
+package example.coroutines
+
+import example.printDone
+import example.shouldBe
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.asContextElement
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+
+suspend fun main() {
+    repeat(500000) {
+        val threadLocal = ThreadLocal<String>()
+        threadLocal.set("value-1")
+        val job = GlobalScope.launch(threadLocal.asContextElement("example")) {
+            threadLocal.get() shouldBe "example"
+
+            threadLocal.set("local-value")
+            threadLocal.get() shouldBe "local-value"
+
+            (0 until 10).map {
+                launch {
+                    threadLocal.get() shouldBe "example"
+
+                    val localVar = (0..100).random()
+                    threadLocal.set("value-$localVar")
+                    threadLocal.get() shouldBe "value-$localVar"
+                }
+            }.joinAll()
+
+            // 로컬에서 값을 변경할 경우 부작용이 발생할 수 있다
+            // returns 'local-value' or 'example'
+            // threadLocal.get() shouldBe "example"
+        }
+        threadLocal.get() shouldBe "value-1"
+        job.join()
+    }
+
+    printDone()
+}
